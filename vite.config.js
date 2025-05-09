50d20415942da3a34407aa27b4eecd6c00f86ac4
Ruth Moraa <moraar37@gmail.com>
2025-05-09 05:42:16 +0300

    adding react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from 'react'
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
