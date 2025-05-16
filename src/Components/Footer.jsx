5bd70f554d6cf4354fc36d03f03750ce8f814291
Ruth Moraa <moraar37@gmail.com>
2025-05-16 04:10:08 +0300

    adding the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+        <h3>
+            This is the footer
+        </h3>
+    </div>
   )
 }
