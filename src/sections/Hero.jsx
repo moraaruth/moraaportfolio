89a6df63b978255fab87dcdf4757682302b79ce0
Ruth Moraa <moraar37@gmail.com>
2025-05-26 04:06:12 +0300

    adding content to hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id='hero' className='relative overflow-hidden'>
+   <div className='absolute top-0 left-0 z-10'>
+   <img src='./assets/public/images/bg.png' alt='background'/>
+   </div>
+
+<div className='hero-layout'>
+  {/* left displaying hero content to the left side */}
+<header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+<div className='flex flex-col gap-7'> 
+<div className='hero-text'>
+<h1>Shaping Designs</h1>
+<h1>into Real Projects</h1>
+<h1>that Deliver Results</h1>
+</div>
+
+</div>
+</header>
+{/* right - 3d model */}
+</div>
+ </section>
   )
 }
 
