30bf644f855bfb4b65c0a6c9e553efa72051eff3
Ruth Moraa <moraar37@gmail.com>
2025-05-26 03:40:35 +0300

    adding position

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './sections/Hero'
 const App = () => {
   return (
  <section id='hero' className='relative overflow-hidden'>
+   <div className='absolute top-0 left-0 z-10'>
+
+   </div>
 
  </section>
   )
