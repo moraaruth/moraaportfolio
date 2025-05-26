880f20c9d69e7d5845c229caaade08e4c2a09bca
Ruth Moraa <moraar37@gmail.com>
2025-05-26 04:28:06 +0300

    adding padding and border raadius

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
           <img 
           src={word.imgPath}
           alt={word.text}
-          className="xl:size-12"
+          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full"
           />
 
         </span>
