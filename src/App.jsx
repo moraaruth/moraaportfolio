c07a049389441f1506cbb427e5355acc9e77752f
Ruth Moraa <moraar37@gmail.com>
2025-05-26 03:42:42 +0300

    adding bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
  <section id='hero' className='relative overflow-hidden'>
    <div className='absolute top-0 left-0 z-10'>
-
+   <img src='/images/bg.png' alt='background'/>
    </div>
 
  </section>
