86e87a209da604790e25fee7d742bb879d84c070
Ruth Moraa <moraar37@gmail.com>
2025-05-27 04:38:58 +0300

    adding more words

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,13 @@ export const words = [
     { text : 'Ideas', imgPath: './assets/public/images/ideas.svg' },
     { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' },
     { text : 'Designs', imgPath: './assets/public/images/designs.svg' },
-    { text : 'Code', imgPath: './assets/public/images/code.svg' }
+    { text : 'Code', imgPath: './assets/public/images/code.svg' },
+    { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' },
+    { text : 'Ideas', imgPath: './assets/public/images/ideas.svg' },
+    { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' },
+    { text : 'Designs', imgPath: './assets/public/images/designs.svg' },
+    { text : 'Code', imgPath: './assets/public/images/code.svg' },
+    { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' },
 
 
   ]
