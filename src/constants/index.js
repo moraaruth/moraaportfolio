d31d59cd6f92fe2c24696ef180ff10d60e8730eb
Ruth Moraa <moraar37@gmail.com>
2025-05-27 04:36:04 +0300

    adding designs

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 export const words = [
     { text : 'Ideas', imgPath: './assets/public/images/ideas.svg' },
     { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' }
-  
+    { text : 'Designs', imgPath: './assets/public/images/designs.svg' }
+
   ]
