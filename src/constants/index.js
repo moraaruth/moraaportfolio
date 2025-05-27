6dc25a51e9461db64f48595414d1d76534dfafa1
Ruth Moraa <moraar37@gmail.com>
2025-05-27 04:36:52 +0300

    adding code

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,8 @@
 export const words = [
     { text : 'Ideas', imgPath: './assets/public/images/ideas.svg' },
-    { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' }
-    { text : 'Designs', imgPath: './assets/public/images/designs.svg' }
+    { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' },
+    { text : 'Designs', imgPath: './assets/public/images/designs.svg' },
+    { text : 'Code', imgPath: './assets/public/images/code.svg' }
+
 
   ]
