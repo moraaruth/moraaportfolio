a5732c97a145291c7be0373db1aaf79ed7be2dbd
Ruth Moraa <moraar37@gmail.com>
2025-05-27 04:41:39 +0300

    formatting

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,14 +1,14 @@
 export const words = [
-    { text : 'Ideas', imgPath: './assets/public/images/ideas.svg' },
-    { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' },
-    { text : 'Designs', imgPath: './assets/public/images/designs.svg' },
-    { text : 'Code', imgPath: './assets/public/images/code.svg' },
-    { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' },
-    { text : 'Ideas', imgPath: './assets/public/images/ideas.svg' },
-    { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' },
-    { text : 'Designs', imgPath: './assets/public/images/designs.svg' },
-    { text : 'Code', imgPath: './assets/public/images/code.svg' },
-    { text : 'Concepts', imgPath: './assets/public/images/concepts.svg' },
+    { text: 'Ideas', imgPath: './assets/public/images/ideas.svg' },
+    { text: 'Concepts', imgPath: './assets/public/images/concepts.svg' },
+    { text: 'Designs', imgPath: './assets/public/images/designs.svg' },
+    { text: 'Code', imgPath: './assets/public/images/code.svg' },
+    { text: 'Concepts', imgPath: './assets/public/images/concepts.svg' },
+    { text: 'Ideas', imgPath: './assets/public/images/ideas.svg' },
+    { text: 'Concepts', imgPath: './assets/public/images/concepts.svg' },
+    { text: 'Designs', imgPath: './assets/public/images/designs.svg' },
+    { text: 'Code', imgPath: './assets/public/images/code.svg' },
+    { text: 'Concepts', imgPath: './assets/public/images/concepts.svg' },
 
 
   ]
