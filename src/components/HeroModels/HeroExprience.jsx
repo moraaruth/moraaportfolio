32c55d469d1f6163f963b2c429e3611b8ebd1e57
Ruth Moraa <moraar37@gmail.com>
2025-05-28 04:45:22 +0300

    adding ambientlight

--- src/components/HeroModels/HeroExprience.jsx
+++ src/components/HeroModels/HeroExprience.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const HeroExprience = () => {
   return (
-  <Canvas camera={{ position: [0, 0, 15], fov: 45 }}> 
+  <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+      <ambientLight intensity={0.2} color="#1a1a40"/> 
 
   </Canvas>
   )
