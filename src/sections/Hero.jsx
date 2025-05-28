38d650bf639cd744842c9b430f67bd6b695b94d9
Ruth Moraa <moraar37@gmail.com>
2025-05-28 04:34:38 +0300

    adding figure

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,55 +2,61 @@ import React from 'react'
 import Button from '../components/Button'
 import { words } from '../constants'
 
-
 const Hero = () => {
   return (
-    <section id='hero' className='relative overflow-hidden'>
-   <div className='absolute top-0 left-0 z-10'>
-   <img src='./assets/public/images/bg.png' alt='background'/>
-   </div>
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="./assets/public/images/bg.png" alt="background" />
+      </div>
 
-<div className='hero-layout'>
-  {/* left displaying hero content to the left side */}
-<header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
-<div className='flex flex-col gap-7'> 
-<div className='hero-text'>
-<h1>Shaping 
-  <span className='slide'>
-    <span className='wrapper'>
-      {words.map((word) => (
-        <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
-          <img 
-          src={word.imgPath}
-          alt={word.text}
-          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
-          />
-          <p>{word.text}</p>
-        </span>
+      <div className="hero-layout">
+        {/* left displaying hero content to the left side */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <p>{word.text}</p>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
 
-      ))}
-
-    </span>
-  </span>
-  </h1>
-
-<h1>into Real Projects</h1>
-<h1>that Deliver Results</h1>
-</div>
-<p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>
-    Hi, I am Ruth  web developer and certified cloud practitioner with passion for digital transformation.
-  </p>
-  <Button 
-  className="md:w-80 md:h-16 w-60 h-12"
-  id="button"
-  text="See my work"
-  
-  />
-</div>
-</header>
-{/* right - 3d model */}
-</div>
- </section>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
+            </div>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I am Ruth web developer and certified cloud practitioner with
+              passion for digital transformation.
+            </p>
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See my work"
+            />
+          </div>
+        </header>
+        {/* right - 3d model */}
+        <figure>
+          <div>
+            
+          </div>
+        </figure>
+      </div>
+    </section>
   )
 }
 
