f39bc9bfb6d55deec08e8cb5e3a83f82c28cc3d9
Ruth Moraa <moraar37@gmail.com>
2025-05-28 05:01:20 +0300

    adding enablePan

--- src/components/HeroModels/HeroExprience.jsx
+++ src/components/HeroModels/HeroExprience.jsx
@@ -7,7 +7,10 @@ const HeroExprience = () => {
   <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       <ambientLight intensity={0.2} color="#1a1a40"/> 
       <directionalLight position={[5, 5, 5]} intensity={1} />
-      <OrbitControls />
+      <OrbitControls 
+      enablePan={false}
+      
+      />
       <mesh>
           <boxGeometry args={[1, 1,1]} />
           <meshStandardMaterial  color="teal"/>
