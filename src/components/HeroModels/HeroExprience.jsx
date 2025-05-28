20ef5a9835981fa0729d23617ff5d5b3c68b17b3
Ruth Moraa <moraar37@gmail.com>
2025-05-28 05:02:15 +0300

    adding dummy varible

--- src/components/HeroModels/HeroExprience.jsx
+++ src/components/HeroModels/HeroExprience.jsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
 const HeroExprience = () => {
+    const isTablet = false;
   return (
   <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       <ambientLight intensity={0.2} color="#1a1a40"/> 
