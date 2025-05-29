f4d3bca35be9b56fe8e2ce2c5bcd5431aab8cb22
Ruth Moraa <moraar37@gmail.com>
2025-05-29 04:46:35 +0300

    adding usemediaquery

--- src/components/HeroModels/HeroExprience.jsx
+++ src/components/HeroModels/HeroExprience.jsx
@@ -1,9 +1,10 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const HeroExprience = () => {
-    const isTablet = false;
+    const isTablet = useMediaQuery({ query: '(max-width: 768)' })
   return (
   <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       <ambientLight intensity={0.2} color="#1a1a40"/> 
