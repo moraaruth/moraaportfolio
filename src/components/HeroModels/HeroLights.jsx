ecf4b94f82fac5332ddb7960eeded500ad288456
Ruth Moraa <moraar37@gmail.com>
2025-05-30 05:17:53 +0300

    changing second spotlight

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -10,6 +10,14 @@ const HeroLights = () => {
  penubra={0.2}
  color="white"
  />
+
+<spotLight 
+ position={[4, 5, 4]}
+ angle={0.15}
+ intensity={100}
+ penubra={0.2}
+ color="white"
+ />
    <ambientLight intensity={0.2} color="#1a1a40"/> 
       <directionalLight position={[5, 5, 5]} intensity={1} />
  
