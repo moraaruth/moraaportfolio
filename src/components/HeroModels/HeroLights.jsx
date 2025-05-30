11e3b1d66e9b6ec5c9988f1ce0586919d6e16443
Ruth Moraa <moraar37@gmail.com>
2025-05-30 05:10:37 +0300

    adding position

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const HeroLights = () => {
   return (
  <>
+ <spotLight 
+ position={[2, 5, 6]}
+ 
+ />
    <ambientLight intensity={0.2} color="#1a1a40"/> 
       <directionalLight position={[5, 5, 5]} intensity={1} />
  
