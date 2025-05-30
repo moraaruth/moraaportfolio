eaa23a0822c8a711fdece626f55bb4071a10eddc
Ruth Moraa <moraar37@gmail.com>
2025-05-30 05:18:38 +0300

    changing intensity and colors

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -13,10 +13,10 @@ const HeroLights = () => {
 
 <spotLight 
  position={[4, 5, 4]}
- angle={0.15}
- intensity={100}
- penubra={0.2}
- color="white"
+ angle={0.3}
+ intensity={40}
+ penubra={0.5}
+ color="4cc9f0"
  />
    <ambientLight intensity={0.2} color="#1a1a40"/> 
       <directionalLight position={[5, 5, 5]} intensity={1} />
