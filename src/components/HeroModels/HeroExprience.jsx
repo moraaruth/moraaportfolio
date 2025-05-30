7758317e97507146e87f220cae05d149e190d146
Ruth Moraa <moraar37@gmail.com>
2025-05-30 05:00:29 +0300

    adding herolights

--- src/components/HeroModels/HeroExprience.jsx
+++ src/components/HeroModels/HeroExprience.jsx
@@ -10,8 +10,7 @@ const HeroExprience = () => {
 
   return (
   <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-      <ambientLight intensity={0.2} color="#1a1a40"/> 
-      <directionalLight position={[5, 5, 5]} intensity={1} />
+    
       <OrbitControls 
       enablePan={false}
       enableZoom={!isTablet}
--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const HeroLights = () => {
   return (
-    <div>HeroLights</div>
+ <>
+   <ambientLight intensity={0.2} color="#1a1a40"/> 
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+ 
+ </>
   )
 }
 
