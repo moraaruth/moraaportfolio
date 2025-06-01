6d66ff4782d1948d2a74e57aeda5375d30e16a07
Ruth Moraa <moraar37@gmail.com>
2025-06-01 06:03:31 +0300

    adding primitive light

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -26,6 +26,10 @@ const HeroLights = () => {
  penubra={1}
  color="9d4edd"
  />
+
+ <primitive 
+ 
+ />
    <ambientLight intensity={0.2} color="#1a1a40"/> 
       <directionalLight position={[5, 5, 5]} intensity={1} />
  
