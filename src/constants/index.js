fe7cc1ac61d527e86981b9e63234c74ce649623d
Ruth Moraa <moraar37@gmail.com>
2025-06-02 12:08:15 +0300

    adding right path to images

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,17 +16,20 @@ const navLinks = [
     link: "#testimonials",
   },
 ];
+export const words = [
+  { text: 'Ideas', imgPath: './assets/public/images/ideas.svg' },
+  { text: 'Concepts', imgPath: './assets/public/images/concepts.svg' },
+  { text: 'Designs', imgPath: './assets/public/images/designs.svg' },
+  { text: 'Code', imgPath: './assets/public/images/code.svg' },
+  { text: 'Concepts', imgPath: './assets/public/images/concepts.svg' },
+  { text: 'Ideas', imgPath: './assets/public/images/ideas.svg' },
+  { text: 'Concepts', imgPath: './assets/public/images/concepts.svg' },
+  { text: 'Designs', imgPath: './assets/public/images/designs.svg' },
+  { text: 'Code', imgPath: './assets/public/images/code.svg' },
+  { text: 'Concepts', imgPath: './assets/public/images/concepts.svg' },
 
-const words = [
-  { text: "Ideas", imgPath: "/images/ideas.svg" },
-  { text: "Concepts", imgPath: "/images/concepts.svg" },
-  { text: "Designs", imgPath: "/images/designs.svg" },
-  { text: "Code", imgPath: "/images/code.svg" },
-  { text: "Ideas", imgPath: "/images/ideas.svg" },
-  { text: "Concepts", imgPath: "/images/concepts.svg" },
-  { text: "Designs", imgPath: "/images/designs.svg" },
-  { text: "Code", imgPath: "/images/code.svg" },
-];
+
+]
 
 const counterItems = [
   { value: 15, suffix: "+", label: "Years of Experience" },
