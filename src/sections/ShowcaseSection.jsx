c853e90882f4509c380fb68a93424b461f368332
Ruth Moraa <moraar37@gmail.com>
2025-06-02 14:08:11 +0300

    adding 3rd correct path

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -76,7 +76,7 @@ const AppShowcase = () => {
 
             <div className="project" ref={ycDirectoryRef}>
               <div className="image-wrapper bg-[#FFE7EB]">
-                <img src="/images/project3.png" alt="YC Directory App" />
+                <img src="./assets/public/images/project3.png" alt="YC Directory App" />
               </div>
               <h2>YC Directory - A Startup Showcase App</h2>
             </div>
