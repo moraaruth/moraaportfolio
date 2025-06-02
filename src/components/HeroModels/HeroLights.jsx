41ad10ed8de5b140ebfc978ceddba41a7a792793
Ruth Moraa <moraar37@gmail.com>
2025-06-02 10:12:46 +0300

    adding position

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -35,7 +35,9 @@ const HeroLights = () => {
  intensity={15}
  rotation={[-Math.PI / 4, Math.PI / 4, 0]}
  />
- <pointLight />
+ <pointLight
+ position={[0, 1 0]}
+ />
    {/* <ambientLight intensity={0.2} color="#1a1a40"/> 
       <directionalLight position={[5, 5, 5]} intensity={1} /> */}
  
