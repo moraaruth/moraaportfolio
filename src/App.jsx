b6a6bf69d692ea5d17e0410c82859f24aa111b54
Ruth Moraa <moraar37@gmail.com>
2025-06-06 06:38:26 +0300

    stling navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import Hero from './sections/Hero';
-import NavBar from './components/NavBar';
+import Hero from './sections/Hero'
+import NavBar from './components/NavBar'
 import ShowcaseSection from './sections/ShowcaseSection'
-import AnimatedCounter from './components/AnimatedCounter';
+import AnimatedCounter from './components/AnimatedCounter'
 
 const App = () => {
   return (
     <>
-    <NavBar />
-    <Hero />
-    <AnimatedCounter />
-<ShowcaseSection />
+      <NavBar />
+      <Hero />
+      <AnimatedCounter />
+      <ShowcaseSection />
     </>
-
-
   )
 }
 
