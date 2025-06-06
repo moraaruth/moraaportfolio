2ad99e037355660ef0c21dc9783aa24e9567f2e0
Ruth Moraa <moraar37@gmail.com>
2025-06-06 06:35:01 +0300

    adding animatedcounter

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from './sections/Hero';
 import NavBar from './components/NavBar';
 import ShowcaseSection from './sections/ShowcaseSection'
+import AnimatedCounter from './components/AnimatedCounter';
 
 const App = () => {
   return (
     <>
     <NavBar />
     <Hero />
+    <AnimatedCounter />
 <ShowcaseSection />
     </>
 
