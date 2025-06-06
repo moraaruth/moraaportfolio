e82105cbee06023d85238916de7293499acb3962
Ruth Moraa <moraar37@gmail.com>
2025-06-06 10:12:55 +0300

    adding contactexprinece

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FeatureCards from './sections/FeatureCards'
 import Experience from './sections/ExprienceSection'
 import TechStack from './sections/TechStack'
 import Testimonials from './sections/Tesimonials'
+import Contact from './sections/Contact'
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Experience />
       <TechStack />
       <Testimonials />
+      <Contact />
     </>
   )
 }
