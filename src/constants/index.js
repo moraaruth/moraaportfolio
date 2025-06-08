29b40dfb1014c219f6b9503d99d8e7e99e44b6e2
Ruth Moraa <moraar37@gmail.com>
2025-06-08 05:45:56 +0300

    adding years of exprience

--- src/constants/index.js
+++ src/constants/index.js
@@ -33,7 +33,7 @@ export const words = [
 ]
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
+  { value: 5, suffix: "+", label: "Years of Experience" },
   { value: 200, suffix: "+", label: "Satisfied Clients" },
   { value: 108, suffix: "+", label: "Completed Projects" },
   { value: 90, suffix: "%", label: "Client Retention Rate" },
