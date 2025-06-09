f089e92da68cbd5e18cef059e3d43b9cf626b987
Ruth Moraa <moraar37@gmail.com>
2025-06-09 21:34:26 +0300

    changing config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/',
   plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+    react()
+  ]
+});
